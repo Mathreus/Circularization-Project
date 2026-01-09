15ed308965933438589f36b5353892f77169b003
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2026-01-09 09:28:33 -0300

    Update e. Vendas para CPF.sql

--- Seleção/e. Vendas para CPF.sql
+++ Seleção/e. Vendas para CPF.sql
@@ -1,1 +1,38 @@
-
+SELECT
+    COD_ESTABELECIMENTO,
+    CASE
+        WHEN COD_ESTABELECIMENTO IN ('R261', 'R221', 'R222', 'R541', 'R591', 'R281', 'R282', 'R282', 'R283', 'R611', 'R121', 'R831', 'R351', 'R352', 'R461', 'R521') THEN 'AVANÇAR'
+        WHEN COD_ESTABELECIMENTO IN ('R201', 'R311', 'R312', 'R313', 'R191', 'R781', 'R301') THEN 'BASE'
+        WHEN COD_ESTABELECIMENTO IN ('R031', 'R041', 'R291', 'R292', 'R641', 'R791', 'R801') THEN 'CRESCER'
+        WHEN COD_ESTABELECIMENTO IN ('R651', 'R671', 'R681', 'R021', 'R181', 'R691', 'R131', 'R721', 'R751') THEN 'FORTALEZA'
+        WHEN COD_ESTABELECIMENTO IN ('R211', 'R341', 'R451', 'R481', 'R711', 'R231', 'R234', 'R471', 'R472', 'R061', 'R531') THEN 'PLANALTO'
+        WHEN COD_ESTABELECIMENTO IN ('R071', 'R074', 'R382', 'R501', 'R502', 'R661', 'R701', 'R491', 'R492', 'R241', 'R243', 'R621', 'R761', 'R371', 'R731', 'R821') THEN 'SUL'
+        WHEN COD_ESTABELECIMENTO IN ('R011', 'R511', 'R101', 'R811', 'R051', 'R052', 'R161') THEN 'VISÃO'
+        ELSE 'CADASTRAR'
+    END AS GRUPO,
+    COD_CLIENTE,
+    NOME_CLIENTE,
+    REPLACE(REPLACE(REPLACE(REPLACE([CPF/CNPJ], '.', ''), '-', ''), '/', ''), ' ', '') AS CPF_CNPJ,
+    LEN(REPLACE(REPLACE(REPLACE(REPLACE([CPF/CNPJ], '.', ''), '-', ''), '/', ''), ' ', '')) AS CARACTERES,
+    DATA_NOTA_FISCAL,
+    NUM_NOTA_FISCAL,
+    COD_ITEM,
+    DESCR_ITEM,
+    QUANTIDADE,
+    VALOR,
+    CFOP,
+    DESC_TIPO_OPERACAO
+FROM    
+    VW_AUDIT_RM_ORDENS_VENDA
+WHERE
+    COD_ESTABELECIMENTO = 'R631'
+    AND DATA_NOTA_FISCAL BETWEEN '2025-07-01' AND '2025-12-31'
+    AND PARA_FATURAMENTO = 'Sim'
+    AND LEN(REPLACE(REPLACE(REPLACE(REPLACE([CPF/CNPJ], '.', ''), '-', ''), '/', ''), ' ', '')) = 11
+    AND NUM_NOTA_FISCAL NOT LIKE '%EST%'
+    AND CFOP IN ('5.101', '5.102', '5.103', '5.104', '5.105', '5.106', '5.107', '5.108', '5.109', 
+                         '5.110', '5.111', '5.112', '5.113', '5.114', '5.115', '5.116', '5.201', '5.202',
+                         '5.203', '5.204', '5.205', '5.206', '5.207', '5.208', '5.209', '5.401', '5.402',
+                         '5.403', '5.404', '5.405', '5.501', '5.502', '5.503', '5.504', '6.101', '6.102',
+                         '6.103', '6.104', '6.105', '6.106', '6.107', '6.108', '6.109', '6.110', '6.111',
+                         '6.112', '6.113', '6.114', '6.115', '6.116')
